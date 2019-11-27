18fa48612fea92f8acd5ae8a6b0f491f68170be8
Fauyan <fauyannn@gmail.com>
2019-11-27 23:16:33 +0700

    WO NG - get item from ng

--- manufacturing_ng/public/js/manufacturing_ng.js
+++ manufacturing_ng/public/js/manufacturing_ng.js
@@ -6,9 +6,32 @@ frappe.ui.form.on('Work Order NG', {
                 ng_dialog(frm);
                 // NotGood
             });
-        }
+		}
+		
+		// setTimeout(function(){
+		// 	$(document).find('.ngitem_dialog input[type="checkbox"]').off();
+		// 	$(document).on('click','.ngitem_dialog input[type="checkbox"]',function(){
+
+				
+		// 		var $this = $(this);
+		// 		var item_code = $.trim($this.closest('.list-item').find('.list-item__content:eq(4)').text());
+		// 		if ($(this).is(':checked')) {
+		// 			console.log($(this).val() + ' is now checked');
+		// 			$this.closest('.modal-dialog').find('input[data-fieldname="item_code"]').val(item_code);
+		// 			$this.closest('.modal-dialog').find('input[data-fieldname="search_term"]').focus();
+		// 			$this.closest('.modal-dialog').find('input[data-fieldname="search_term"]').trigger($.Event("keypress", { keyCode: 65 })); //spasi = 32
+		// 			// $this.closest('.modal-dialog').find('input[data-fieldname="search_term"]').trigger($.Event("keypress", { keyCode: 8 }));
+
+		// 		} else {
+		// 			console.log($(this).val() + ' is now unchecked');
+		// 			// $this.closest('.modal-dialog').find('input[data-fieldname="item_code"]').val('');
+		// 		}
+		// 	});
+		// },1)
+		
     }
 }); 
+
 
 // let manufacturing_ng = manufacturing_ng || {};
 
@@ -20,12 +43,13 @@ var NotGoodDialog = frappe.ui.form.MultiSelectDialog.extend({
 		this._super(results, more)
 		
 		this.dialog.$wrapper.find('.modal-dialog').css({'width': "90%"});
+		this.dialog.$wrapper.find('.modal-dialog').addClass("ngitem_dialog");
 	}
 });
 
 
 function ng_dialog(frm){
-    console.log(frm.doc)
+    // console.log(frm.doc)
 	var ng_dialog = new NotGoodDialog({
 		doctype: "Not Good",
 		target: cur_frm,
@@ -84,10 +108,11 @@ function ng_dialog(frm){
 				label: "Quantity"
 			}
 		],
-		get_query: function(){
+		get_query_filters: function(){
+			// console.log(1111111)
 			return {
 				filters: {
-					// customer: cur_frm.doc.customer
+					// item_code: cur_frm.doc.item_code
 				}
 			}
 		},
@@ -96,27 +121,58 @@ function ng_dialog(frm){
 				frappe.msgprint(__("Please select Item"))
 				return;
 			}
-
-			if($.isArray(cur_frm.doc.items) && cur_frm.doc.items.length > 0) {
-				if(!cur_frm.doc.items[0].item_code) {
-					cur_frm.doc.items = cur_frm.doc.items.splice(1);
+			// console.log(selections)
+			if($.isArray(cur_frm.doc.required_items) && cur_frm.doc.required_items.length > 0) {
+				if(!cur_frm.doc.required_items[0].item_code) {
+					cur_frm.doc.required_items = cur_frm.doc.required_items.splice(1);
 				}
 			}
-            
+            // console.log(cur_frm.doc.required_items)
 			var item_table;
+			var item_qty = 0;
+			var item_code;
+			var i = 0;
+			var WO;
 			$.each(selections, function(i, value){
+				i++;
 				frappe.model.with_doc("Not Good", value, function(){
                     var item = frappe.model.get_doc("Not Good", value);
-                    console.log(frm.doc.name)
-                    frappe.model.set_value('Work Order NG', frm.doc.name, "production_item", item.item_code);
-					if(cur_frm.doc.required_items.filter((obj) => obj.item_code == item.item_code && obj.schedule_date == item.delivery_date).length == 0) {
-						item_table = cur_frm.add_child("required_items");
-						frappe.model.set_value(item_table.doctype, item_table.name, "item_code", item.item_code);
-						frappe.model.set_value(item_table.doctype, item_table.name, "qty", item.qty);
-						frappe.model.set_value(item_table.doctype, item_table.name, "schedule_date", item.delivery_date);
+					// console.log(item)
+					WO = item.work_order;
+                    item_code = item.item_code;
+					item_qty += parseInt(item.quantity);
+					// if(cur_frm.doc.required_items.filter((obj) => obj.item_code == item.item_code && obj.schedule_date == item.delivery_date).length == 0) {
+					// 	item_table = cur_frm.add_child("required_items");
+					// 	frappe.model.set_value(item_table.doctype, item_table.name, "item_code", item.item_code);
+					// 	frappe.model.set_value(item_table.doctype, item_table.name, "qty", item.qty);
+					// 	frappe.model.set_value(item_table.doctype, item_table.name, "schedule_date", item.delivery_date);
+					// }
+					// console.log(selections.length +' == '+ i);
+					if(selections.length == i){						
+						frappe.model.set_value('Work Order NG', frm.doc.name, "production_item", item_code);
+						frappe.model.set_value('Work Order NG', frm.doc.name, "qty", item_qty);
 					}
+					
 				})
 			});
+			// var woi = frappe.model.get_doc('Work Order Item')
+			
+			// console.log(frm.doc.required_items)
+
+			var not_good_warehouse = '';
+			setTimeout(function(){
+				frappe.model.with_doc("Work Order", WO, function(){
+					not_good_warehouse = frappe.model.get_value("Work Order", WO,'not_good_warehouse');
+					// console.log(not_good_warehouse)
+				})
+			},1000)
+			
+			setTimeout(function(){
+				$.each(frm.doc.required_items, function(k,val){
+					frappe.model.set_value('Work Order Item', val.name, "source_warehouse", not_good_warehouse) // Not Good Warehouse					
+				})
+			},2000)
+
 			ng_dialog.dialog.hide();
 		}
 	});
